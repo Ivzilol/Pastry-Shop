3416231edaf6271350bd8588efccbcd472e9a204
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-01-28 19:18:38 +0200

    UseLocalState

--- src/pastry-shob-web/src/App.js
+++ src/pastry-shob-web/src/App.js
@@ -2,35 +2,12 @@ import './App.css';
 import React, {useEffect} from "react";
 import {Route, Routes} from "react-router-dom";
 import Dashboard from "./components/dashboard/Dashboard";
-import {useLocalState} from "./util/useLocalStorage";
 import Homepage from "./components/homepage/Homepage";
 import Login from "./components/Login/Login";
 import PrivateRoute from "./PriviteRoute/PrivateRoute";
 
 
 function App() {
-
-    const [jwt, setJwt] = useLocalState("", "jwt")
-
-    // useEffect(() => {
-    //     if (!jwt) {
-    //         const requestBody = {
-    //             "username": "Ivzilol",
-    //             "password": "asdasd",
-    //         }
-    //         fetch("api/auth/login", {
-    //             headers: {
-    //                 "Content-Type": "application/json"
-    //             },
-    //             method: "post",
-    //             body: JSON.stringify(requestBody)
-    //         }).then(response => Promise.all([response.json(), response.headers]))
-    //             .then(([body, headers]) => {
-    //                 setJwt(headers.get("authorization"));
-    //             });
-    //     }
-    // }, []);
-
     return (
         <Routes>
             <Route path="/dashboard" element={
--- src/pastry-shob-web/src/components/Login/Login.js
+++ src/pastry-shob-web/src/components/Login/Login.js
@@ -1,15 +1,63 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useLocalState} from "../../util/useLocalStorage";
 
 const Login = () => {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [jwt, setJwt] = useLocalState("", "jwt")
+
+    function sendLoginRequest() {
+        const requestBody = {
+            "username": username,
+            "password": password,
+        }
+        fetch("api/auth/login", {
+            headers: {
+                "Content-Type": "application/json"
+            },
+            method: "post",
+            body: JSON.stringify(requestBody)
+        }).then(response => {
+            if (response.status === 200) {
+                return Promise.all([response.json(), response.headers])
+            } else {
+                return Promise.reject("Invalid Login")
+            }
+        })
+            .then(([body, headers]) => {
+                setJwt(headers.get("authorization"));
+                window.location.href = "dashboard"
+            }).catch((message) => {
+            alert(message)
+        });
+    }
+
     return (
         <>
             <div>
                 <label htmlFor="username">Username</label>
-                <input type="email" id="username"/>
+                <input
+                    type="email"
+                    id="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
             </div>
             <div>
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password"/>
+                <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </div>
+            <div>
+                <button id="submit" type="button" onClick={() => sendLoginRequest()}>
+                    Login
+                </button>
             </div>
         </>
     );
