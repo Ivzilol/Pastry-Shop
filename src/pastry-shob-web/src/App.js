ac977a6db6ae42d112ba642c80fa07560b4c784c
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-01-28 16:49:53 +0200

    Add Routs

--- src/pastry-shob-web/src/App.js
+++ src/pastry-shob-web/src/App.js
@@ -3,47 +3,38 @@ import React, {useEffect} from "react";
 import {Route, Routes} from "react-router-dom";
 import Dashboard from "./components/dashboard/Dashboard";
 import {useLocalState} from "./util/useLocalStorage";
+import Homepage from "./components/homepage/Homepage";
 
 
 function App() {
 
     const [jwt, setJwt] = useLocalState("", "jwt")
 
-    useEffect(() => {
-        if (!jwt) {
-            const requestBody = {
-                "username": "Ivzilol",
-                "password": "asdasd",
-            }
-            fetch("api/auth/login", {
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                method: "post",
-                body: JSON.stringify(requestBody)
-            }).then(response => Promise.all([response.json(), response.headers]))
-                .then(([body, headers]) => {
-                    setJwt(headers.get("authorization"));
-                    console.log(jwt)
-                });
-        }
-    }, []);
+    // useEffect(() => {
+    //     if (!jwt) {
+    //         const requestBody = {
+    //             "username": "Ivzilol",
+    //             "password": "asdasd",
+    //         }
+    //         fetch("api/auth/login", {
+    //             headers: {
+    //                 "Content-Type": "application/json"
+    //             },
+    //             method: "post",
+    //             body: JSON.stringify(requestBody)
+    //         }).then(response => Promise.all([response.json(), response.headers]))
+    //             .then(([body, headers]) => {
+    //                 setJwt(headers.get("authorization"));
+    //             });
+    //     }
+    // }, []);
 
     return (
-        // <Routes>
-        //     <Route path="/dashboard" element={<Dashboard/>}/>
-        //     <Route path="/" element={() => {
-        //         return <div>Pastry-Shop</div>
-        //     }
-        //     }>
-        //
-        //     </Route>
-        // </Routes>
-
-        <div>
-            <h1>Pastry Shop</h1>
-            <div>JWT Values is {jwt}</div>
-        </div>
+        <Routes>
+            <Route path="/dashboard" element={<Dashboard/>}/>
+            <Route path="/" element={<Homepage/>}>
+            </Route>
+        </Routes>
     );
 }
 
