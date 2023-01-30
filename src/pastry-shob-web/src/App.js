d3f6d9a4e8417930363ceb37ff415fecbaac25b1
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-01-30 14:23:26 +0200

    Use bootstrap

--- src/pastry-shob-web/src/App.js
+++ src/pastry-shob-web/src/App.js
@@ -6,7 +6,7 @@ import Homepage from "./components/homepage/Homepage";
 import Login from "./components/Login/Login";
 import PrivateRoute from "./PriviteRoute/PrivateRoute";
 import ShopsView from "./ShopsView/ShopsView";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return (
--- src/pastry-shob-web/src/components/Login/Login.js
+++ src/pastry-shob-web/src/components/Login/Login.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {useLocalState} from "../../util/useLocalStorage";
+import {Button, Col, Container, Row, Form} from "react-bootstrap";
 
 const Login = () => {
 
@@ -37,31 +38,42 @@ const Login = () => {
 
     return (
         <>
-            <div>
-                <label htmlFor="username">Username</label>
-                <input
-                    type="email"
-                    id="username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-            </div>
-            <div>
-                <label htmlFor="password">Password</label>
-                <input
-                    type="password"
-                    id="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-            </div>
-            <div>
-                <button id="submit" type="button" onClick={() => sendLoginRequest()}>
-                    Login
-                </button>
-            </div>
+            <Container className="mt-sm-5">
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label htmlFor="username" className="fs-3">Username</Form.Label>
+                        <Form.Control
+                            type="email"
+                            id="username"
+                            placeholder="John Doe"
+                            size="lg"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+
+                                <Form.Label htmlFor="password" className="fs-3">Password</Form.Label>
+                                <Form.Control
+                                    type="password"
+                                    id="password"
+                                    placeholder="password"
+                                    size="lg"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                </Form.Group>
+                                <Button
+                                    className="fs-5, mt-sm-3"
+                                    id="submit"
+                                    type="button"
+                                    onClick={() => sendLoginRequest()}
+                                >
+                                    Login
+                                </Button>
+            </Container>
         </>
-    );
+);
 };
 
 export default Login;
