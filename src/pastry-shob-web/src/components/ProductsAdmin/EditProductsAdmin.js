c58b252e376eb2e8ff86c5b97d735cbef5eebbba
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-03-13 21:29:38 +0200

    Edit Product

--- src/pastry-shob-web/src/components/ProductsAdmin/EditProductsAdmin.js
+++ src/pastry-shob-web/src/components/ProductsAdmin/EditProductsAdmin.js
@@ -8,14 +8,7 @@ const EditProductsAdmin = () => {
     const user = useUser();
     const {productId} = useParams();
 
-    const [product, setProduct] = useState({
-        nameProduct: "",
-        price: null,
-        categories: "",
-        description: "",
-        image: "",
-        shopName: ""
-    })
+    const [product, setProduct] = useState(null)
 
     useEffect(() => {
         ajax(`/api/products/${productId}`, "GET", user.jwt)
@@ -36,8 +29,42 @@ const EditProductsAdmin = () => {
                         <article className="products-edit-container">
                             <h5 className="products-edit-item">Name Product</h5>
                             <input
-                                // onChange={(e ) => updateProduct("nameProduct", e.target.value)}
-                                value={product.nameProduct}
+                                onChange={(e) => updateProduct("name", e.target.value)}
+                                defaultValue={product.name}
+                                name="name"
+                                id="name"
+                                type="text"
+                            />
+                            <h5 className="products-edit-item">Price</h5>
+                            <input
+                                onChange={(e) => updateProduct("price", e.target.value)}
+                                defaultValue={product.price}
+                                name="price"
+                                id="price"
+                                type="number"
+                            />
+                            <h5 className="products-edit-item">Categories</h5>
+                            <input
+                                onChange={(e) => updateProduct("categories", e.target.value)}
+                                defaultValue={product.categories}
+                                name="categories"
+                                id="categories"
+                                type="text"
+                            />
+                            <h5 className="products-edit-item">Description</h5>
+                            <input
+                                onChange={(e) => updateProduct("description", e.target.value)}
+                                defaultValue={product.description}
+                                name="description"
+                                id="description"
+                                type="text"
+                            />
+                            <h5 className="products-edit-item">URL</h5>
+                            <input
+                                onChange={(e) => updateProduct("imageUrl", e.target.value)}
+                                defaultValue={product.imageUrl}
+                                name="imageUrl"
+                                id="imageUrl"
                                 type="text"
                             />
                         </article>
