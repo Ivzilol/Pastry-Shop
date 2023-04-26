8934454a7bd9d414521a3aa7672e79d2f2a26115
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-04-26 12:48:23 +0300

    Home page

--- src/pastry-shob-web/src/components/homepage/Homepage.js
+++ src/pastry-shob-web/src/components/homepage/Homepage.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import NavBar from "../NavBar/NavBar";
+import {useUser} from "../../UserProvider/UserProvider";
+import {useNavigate} from "react-router-dom";
+import ajax from "../../Services/FetchService";
 
 const Homepage = () => {
+
+    const user = useUser();
+    const [products, setProducts] = useState(null);
+    let navigate = useNavigate();
+    const [product, setProduct] = useState({
+            name: "",
+            price: null,
+            categories: "",
+            description: "",
+            imageUrl: ""
+        }
+    );
+
+    useEffect(() => {
+        ajax("api/", "GET", user.jwt)
+            .then(productsData => {
+                setProduct(productsData);
+            })
+        if (!user.jwt) navigate("/login")
+    }, [user.jwt]);
+
     return (
         <>
             <NavBar/>
