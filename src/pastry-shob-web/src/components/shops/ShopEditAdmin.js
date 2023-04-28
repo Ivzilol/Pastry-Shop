eab7d2af424a91d3741532abf9ca36b9793ec9ed
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-04-28 13:55:36 +0300

    Edit Shop Admin

--- src/pastry-shob-web/src/components/shops/ShopEditAdmin.js
+++ src/pastry-shob-web/src/components/shops/ShopEditAdmin.js
@@ -1,6 +1,6 @@
 import {useUser} from "../../UserProvider/UserProvider";
 import {useNavigate} from "react-router-dom";
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import ajax from "../../Services/FetchService";
 
 const ShopEditAdmin = () => {
@@ -17,6 +17,7 @@ const ShopEditAdmin = () => {
 
     const [shopEnum, setShopEnum] = useState([]);
     const [shopStatus, setShopStatus] = useState([]);
+    const prevShopVale = useRef(shop);
 
     useEffect(() => {
         ajax(`/api/shops/${shopId}`, "GET", user.jwt)
@@ -27,14 +28,72 @@ const ShopEditAdmin = () => {
                 setShop(shopData);
                 setShopEnum(shopResponse.shopsEnums);
                 setShopStatus(shopResponse.statusEnums);
+            });
+    }, []);
+
+    function updateShop(prop, value) {
+        const newShop = {...shop};
+        newShop[prop] = value;
+        setShop(newShop);
+    }
+
+    function editShop() {
+        ajax(`/api/shops/${shopId}`, "PUT", user.jwt, shop)
+            .then(shopData => {
+                setShop(shopData);
             })
-    })
+    }
 
     return (
         <main className="shop-edit-admin">
             {shop ? (
-                <section>
+                <section className="shop-edit-admin-container">
+                    {shop ? (
+                        <div className="shop-edit-admin-container-items">
+                            <article className="shop-edit-admin-container-item">
+                                <h6>Name:</h6>
+                                <label>
+                                <input
+                                    onChange={(e) => updateShop("name", e.target.value)}
+                                    value={shop.name}
+                                    type="text"
+                                    name="name"
+                                />
+                                </label>
+                            </article>
+                            <article className="shop-edit-admin-container-item">
+                                <h6>Town:</h6>
+                                <label>
+                                <input
+                                    onChange={(e) => updateShop("town", e.target.value)}
+                                    value={shop.town}
+                                    type="text"
+                                    name="town"
+                                />
+                                </label>
+                            </article>
+                            <article className="shop-edit-admin-container-item">
+                                <h6>Address:</h6>
+                                <label>
+                                <input
+                                    onChange={(e) => updateShop("address", e.target.value)}
+                                    value={shop.address}
+                                    type="text"
+                                    name="address"
+                                />
+                                </label>
+                            </article>
+                            <section className="shop-edit-admin-container-item-buttons">
+                                <button
+                                    type="submit"
+                                    onClick={() => editShop()}
+                                >Change Shop</button>
+                            </section>
+                        </div>
 
+                    ) : (
+                        <></>
+                    )}
                 </section>
             ) : (
                 <></>
