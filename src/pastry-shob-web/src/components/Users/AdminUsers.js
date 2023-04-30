41573969bcff3063c908f66d997a56ec7284f597
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-04-30 15:54:14 +0300

    Users control Admin

--- src/pastry-shob-web/src/components/Users/AdminUsers.js
+++ src/pastry-shob-web/src/components/Users/AdminUsers.js
@@ -15,8 +15,37 @@ const AdminUsers = () => {
     }, [user.jwt])
 
     return (
-        <main>
-
+        <main className="admin-users-container">
+            <div className="admin-users-container-header">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Username</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Email</th>
+                            <th>Address</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+            <div className="admin-users-container-content">
+                {users.map(user => (
+                    <tbody className="admin-users-container-content-tbody"
+                    id={user.id}
+                    >
+                        <tr>
+                            <td>{user.id}</td>
+                            <td>{user.username}</td>
+                            <td>{user.firstName}</td>
+                            <td>{user.lastName}</td>
+                            <td>{user.email}</td>
+                            <td>{user.address}</td>
+                        </tr>
+                    </tbody>
+                ))}
+            </div>
         </main>
     )
 }
