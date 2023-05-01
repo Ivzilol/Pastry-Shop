65ea645dfb6238f4d43c9e9ccd7102246d5314a7
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-05-01 22:05:05 +0300

    Users edit User

--- src/main/java/com/example/pastry/shop/controllers/UserController.java
+++ src/main/java/com/example/pastry/shop/controllers/UserController.java
@@ -25,7 +25,6 @@ public class UserController {
 
     private final UserService userService;
     private final AuthenticationManager authenticationManager;
-
     private final JwtUtil jwtUtil;
 
 
--- src/main/java/com/example/pastry/shop/model/entity/Users.java
+++ src/main/java/com/example/pastry/shop/model/entity/Users.java
@@ -8,7 +8,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Set;
 
 @Entity
 @Table(name = "users")
