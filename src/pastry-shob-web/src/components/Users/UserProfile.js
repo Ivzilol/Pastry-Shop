cf48a8021acf215bc4b0dce299ca5a408d250a05
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-05-01 10:57:03 +0300

    Users control User

--- src/pastry-shob-web/src/components/Users/UserProfile.js
+++ src/pastry-shob-web/src/components/Users/UserProfile.js
@@ -19,12 +19,20 @@ const UserProfile = () => {
 
     return (
         <main className="user-profile">
+            <h3 className="user-profile">Your personal info</h3>
             {currentUser ? (
-                <section className="user-profile-container">
-                    <h6>{currentUser.username}</h6>
-                    <p>{currentUser.firstName}</p>
-                    <p>{currentUser.lastName}</p>
-                    <p>{currentUser.email}</p>
+                <section
+                    id={currentUser.id}
+                    className="user-profile-container">
+                    <h6>Username: {currentUser.username}</h6>
+                    <p>First Name: {currentUser.firstName}</p>
+                    <p>Last Name: {currentUser.lastName}</p>
+                    <p>Email: {currentUser.email}</p>
+                    <button
+                    onClick={() => {
+                        window.location.href = `/users/${currentUser.id}`;
+                    }}
+                    >Edit profile</button>
                 </section>
             ) : (
                 <></>
