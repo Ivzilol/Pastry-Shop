ec9e1f1af77155352d5d36b16729cb0ba3840591
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-05-08 21:57:07 +0300

    Tests

--- src/test/java/com/example/pastry/shop/controllerTest/UsersControllerIntegrationTest.java
+++ src/test/java/com/example/pastry/shop/controllerTest/UsersControllerIntegrationTest.java
@@ -5,6 +5,7 @@ import com.example.pastry.shop.model.entity.Authority;
 import com.example.pastry.shop.model.entity.Users;
 import com.example.pastry.shop.testRepository.TestH2RepositoryAuthority;
 import com.example.pastry.shop.testRepository.TestH2RepositoryUsers;
+import org.apache.catalina.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,6 +16,9 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UsersControllerIntegrationTest {
@@ -87,7 +91,7 @@ public class UsersControllerIntegrationTest {
 //    }
 
     @Test
-    public void testGetAllUsers() {
+    public void testRegisterUsers() {
         UserRegistrationDTO registrationDto = new UserRegistrationDTO();
         registrationDto.setUsername("Tosho");
         registrationDto.setFirstName("Georgi");
@@ -108,10 +112,10 @@ public class UsersControllerIntegrationTest {
         registrationDto2.setPassword("asdasd");
         registrationDto2.setConfirmPassword("asdasd");
         Users response2 = restTemplate.postForObject(baseUrl + "/register", registrationDto2, Users.class);
+        Assertions.assertEquals("Tosho", response.getUsername());
+        Assertions.assertEquals("Victor", response2.getUsername());
         Assertions.assertEquals(2, testH2RepositoryUsers.findAll().size());
         Assertions.assertEquals(2, testH2RepositoryAuthority.findAll().size());
-        Assertions.assertEquals("Sofiq", response.getAddress());
-        Assertions.assertEquals("Sofiq", response2.getAddress());
         List<Authority> admins = testH2RepositoryAuthority.findAll()
                 .stream().filter(a -> a.getAuthority().equals("admin")).toList();
         List<Authority> users = testH2RepositoryAuthority.findAll()
@@ -120,9 +124,43 @@ public class UsersControllerIntegrationTest {
         Assertions.assertEquals(1, users.size());
         List<Users> allUsers = testH2RepositoryUsers.findAll();
         Assertions.assertEquals(2, allUsers.size());
+        String id = "{\"{id}\"}";
+        Long currentId = 1L;
+        Users byId = restTemplate.getForObject(baseUrl + "/{id}", Users.class, currentId);
+        Assertions.assertEquals("Tosho", byId.getUsername());
+        Optional<Users> userForFind = testH2RepositoryUsers.findAll().stream()
+                .filter(u -> u.getId().equals(1L)).findFirst();
+        Users currentUser = restTemplate.getForObject(baseUrl, Users.class);
+        Assertions.assertEquals(response, currentUser);
+
+    }
+
+    @Test
+    public void testRepoSize() {
+        Assertions.assertEquals(2, testH2RepositoryUsers.findAll().size());
+        Assertions.assertEquals(2, testH2RepositoryAuthority.findAll().size());
+    }
+
+    @Test
+    public void testTypeUser() {
+        List<Authority> admins = testH2RepositoryAuthority.findAll()
+                .stream().filter(a -> a.getAuthority().equals("admin")).toList();
+        List<Authority> users = testH2RepositoryAuthority.findAll()
+                .stream().filter(a -> a.getAuthority().equals("user")).toList();
+        Assertions.assertEquals(1, admins.size());
+        Assertions.assertEquals(1, users.size());
+    }
+
+    @Test
+    public void testGetAllUsers() {
+        List<Users> allUsers = testH2RepositoryUsers.findAll();
+        Assertions.assertEquals(2, allUsers.size());
+    }
+
+    @Test
+    public void testGetUserByUsername() {
         Users userById = testH2RepositoryUsers.findAll().stream()
                 .filter(u -> u.getUsername().equals("Tosho")).findFirst().orElse(null);
         Assertions.assertEquals("Tosho", userById.getUsername());
-
     }
 }
