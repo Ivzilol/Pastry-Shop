ffb8a0c886016c4294210e8a41e37b71f95a91b1
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-05-08 10:37:21 +0300

    Create Admin

--- src/test/java/com/example/pastry/shop/controllerTest/UsersControllerIntegrationTest.java
+++ src/test/java/com/example/pastry/shop/controllerTest/UsersControllerIntegrationTest.java
@@ -1,7 +1,9 @@
 package com.example.pastry.shop.controllerTest;
 
 import com.example.pastry.shop.model.dto.UserRegistrationDTO;
+import com.example.pastry.shop.model.entity.Authority;
 import com.example.pastry.shop.model.entity.Users;
+import com.example.pastry.shop.model.enums.AuthorityEnum;
 import com.example.pastry.shop.testRepository.TestH2RepositoryUsers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
@@ -11,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.web.client.RestTemplate;
+
+import static org.apache.coyote.http11.Constants.a;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UsersControllerIntegrationTest {
@@ -55,5 +59,28 @@ public class UsersControllerIntegrationTest {
         Assertions.assertEquals("Sofiq", response.getAddress());
         Assertions.assertEquals("0887778899", response.getPhoneNumber());
         Assertions.assertEquals(1, testH2RepositoryUsers.findAll().size());
+    }
+
+    @Test
+    public void testCreateAdmin() {
+        UserRegistrationDTO registrationDto = new UserRegistrationDTO();
+        registrationDto.setUsername("Tosho");
+        registrationDto.setFirstName("Georgi");
+        registrationDto.setLastName("Georgiev");
+        registrationDto.setEmail("gundi@abv.bg");
+        registrationDto.setAddress("Sofiq");
+        registrationDto.setPhoneNumber("0887778899");
+        registrationDto.setPassword("bbGGbb123");
+        registrationDto.setConfirmPassword("bbGGbb123");
+        Users response = restTemplate.postForObject(baseUrl, registrationDto, Users.class, Authority.class);
+        Assertions.assertEquals("Tosho", response.getUsername());
+        Assertions.assertEquals("gundi@abv.bg", response.getEmail());
+        Assertions.assertEquals("Georgi", response.getFirstName());
+        Assertions.assertEquals("Georgiev", response.getLastName());
+        Assertions.assertEquals("Sofiq", response.getAddress());
+        Assertions.assertEquals("0887778899", response.getPhoneNumber());
+        Assertions.assertEquals(1, testH2RepositoryUsers.findAll().size());
+        Assertions.assertFalse(response.getAuthorities().stream()
+                .anyMatch(auth -> AuthorityEnum.admin.name().equals(auth.getAuthority())));
     }
 }
