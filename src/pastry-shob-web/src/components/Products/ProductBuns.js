e68b4eafeb63ba6361828de8a080592fdabe5f84
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-05-11 13:54:39 +0300

    Page Categories Buns

--- src/pastry-shob-web/src/components/Products/ProductBuns.js
+++ src/pastry-shob-web/src/components/Products/ProductBuns.js
@@ -3,6 +3,7 @@ import {useUser} from "../../UserProvider/UserProvider";
 import {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import ajax from "../../Services/FetchService";
+import Footer from "../Footer/Footer";
 
 const ProductBuns = () => {
     const user = useUser()
@@ -20,6 +21,60 @@ const ProductBuns = () => {
     return (
         <main>
             <NavBar/>
+            <section className="pies-container">
+                {buns ? (
+                    <article className="products-container">
+                        {buns.map((product) => (
+                            <div
+                                className="products-container-items"
+                                key={product.id}
+                                id={product.id}
+                            >
+                                <p className="products-container-item"
+                                >Продукт: {product.name}</p>
+                                <p className="products-container-item"
+                                >Цена: {product.price} лв.</p>
+                                <p className="products-container-item"
+                                >Описание на продукта: {product.description}</p>
+                                <img
+                                    className="product-img" src={product.imageUrl} alt="new"
+                                />
+                                {/*<div className="products-container-item-likes-container">*/}
+                                {/*    <p className="products-container-item-likes">*/}
+                                {/*        <FontAwesomeIcon icon={faThumbsUp}*/}
+                                {/*                         className="products-container-item-likes-icon"*/}
+                                {/*        />*/}
+                                {/*        {Number(product.userLikes.length)}</p>*/}
+                                {/*    <div className="products-container-item-likes-container-buttons">*/}
+                                {/*        <button*/}
+                                {/*            className="products-container-item-likes-button"*/}
+                                {/*            id="submit"*/}
+                                {/*            type="button"*/}
+                                {/*            onClick={() => likeProduct(product.id)}*/}
+                                {/*        >Харесва ми*/}
+                                {/*        </button>*/}
+
+                                {/*        <button*/}
+                                {/*            className="products-container-item-likes-container-button2"*/}
+                                {/*            id="submit"*/}
+                                {/*            type="button"*/}
+                                {/*            onClick={() => {*/}
+                                {/*                orderProduct(product.id);*/}
+                                {/*                orderProducts(product.id);*/}
+                                {/*            }}*/}
+                                {/*        >*/}
+                                {/*            Поръчай*/}
+                                {/*        </button>*/}
+                                {/*    </div>*/}
+                                {/*</div>*/}
+                            </div>
+                        ))}
+                    </article>
+                ) : (
+                    <></>
+                )}
+            </section>
+            <Footer/>
         </main>
     )
 }
