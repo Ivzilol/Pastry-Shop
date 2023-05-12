2c5304de1fb407553f24a1782b0dea002490101f
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-05-12 16:16:45 +0300

    Test frond-end app

--- src/pastry-shob-web/src/components/Author/__test__/Author.test.js
+++ src/pastry-shob-web/src/components/Author/__test__/Author.test.js
@@ -12,3 +12,22 @@ test('find name in role', () => {
     const headingElement = screen.getByRole("heading");
     expect(headingElement).toBeInTheDocument();
 });
+
+test('render author image', () => {
+    render(<Author/>)
+    const authorImage = screen.getByAltText("new")
+    expect(authorImage).toBeInTheDocument()
+});
+
+test('render author bio', () => {
+    render(<Author/>)
+    const authorBio = screen.getByText(/Здравйте, казвам се Иво и съм автора на приложението на Мама/i)
+    expect(authorBio).toBeInTheDocument()
+});
+
+test("should render author's contact information", () => {
+    render(<Author/>)
+    const contactInfo = screen.getByText(/Здравйте, казвам се Иво и съм автора на приложението на Мама/i)
+    expect(contactInfo).toBeInTheDocument()
+});
+
