f9719ac4e058e5d463a207d53bf054880f0e3351
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-05-19 13:45:33 +0300

    Refactoring

--- src/test/java/com/example/pastry/shop/testRepository/TestH2RepositoryUsers.java
+++ src/test/java/com/example/pastry/shop/testRepository/TestH2RepositoryUsers.java
@@ -1,10 +1,10 @@
-package com.example.pastry.shop.testRepository;
-
-import com.example.pastry.shop.model.entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface TestH2RepositoryUsers extends JpaRepository<Users, Long> {
-
-}
+//package com.example.pastry.shop.testRepository;
+//
+//import com.example.pastry.shop.model.entity.Users;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.List;
+//
+//public interface TestH2RepositoryUsers extends JpaRepository<Users, Long> {
+//
+//}
--- src/test/java/com/example/pastry/shop/util/TestDataUtils.java
+++ src/test/java/com/example/pastry/shop/util/TestDataUtils.java
@@ -1,32 +1,32 @@
-package com.example.pastry.shop.util;
-
-import com.example.pastry.shop.repository.*;
-import org.springframework.stereotype.Component;
-
-@Component
-public class TestDataUtils {
-
-    private final AuthorityRepository authorityRepository;
-
-    private final CommentRepository commentRepository;
-
-    private final OrdersProcessingRepository ordersProcessingRepository;
-
-    private final OrdersRepository ordersRepository;
-
-    private final ProductRepository productRepository;
-
-    private final ShopsRepository shopsRepository;
-
-    private final UsersRepository usersRepository;
-
-    public TestDataUtils(AuthorityRepository authorityRepository, CommentRepository commentRepository, OrdersProcessingRepository ordersProcessingRepository, OrdersRepository ordersRepository, ProductRepository productRepository, ShopsRepository shopsRepository, UsersRepository usersRepository) {
-        this.authorityRepository = authorityRepository;
-        this.commentRepository = commentRepository;
-        this.ordersProcessingRepository = ordersProcessingRepository;
-        this.ordersRepository = ordersRepository;
-        this.productRepository = productRepository;
-        this.shopsRepository = shopsRepository;
-        this.usersRepository = usersRepository;
-    }
-}
+//package com.example.pastry.shop.util;
+//
+//import com.example.pastry.shop.repository.*;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class TestDataUtils {
+//
+//    private final AuthorityRepository authorityRepository;
+//
+//    private final CommentRepository commentRepository;
+//
+//    private final OrdersProcessingRepository ordersProcessingRepository;
+//
+//    private final OrdersRepository ordersRepository;
+//
+//    private final ProductRepository productRepository;
+//
+//    private final ShopsRepository shopsRepository;
+//
+//    private final UsersRepository usersRepository;
+//
+//    public TestDataUtils(AuthorityRepository authorityRepository, CommentRepository commentRepository, OrdersProcessingRepository ordersProcessingRepository, OrdersRepository ordersRepository, ProductRepository productRepository, ShopsRepository shopsRepository, UsersRepository usersRepository) {
+//        this.authorityRepository = authorityRepository;
+//        this.commentRepository = commentRepository;
+//        this.ordersProcessingRepository = ordersProcessingRepository;
+//        this.ordersRepository = ordersRepository;
+//        this.productRepository = productRepository;
+//        this.shopsRepository = shopsRepository;
+//        this.usersRepository = usersRepository;
+//    }
+//}
