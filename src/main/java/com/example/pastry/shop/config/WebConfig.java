d618b3708c7b8df9880fbb735215379c67a28eca
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-05-27 10:41:10 +0300

    Refactoring

---
+++ src/main/java/com/example/pastry/shop/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.example.pastry.shop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000/")
+                .allowedMethods("PUT", "DELETE")
+                .allowedHeaders("Access-Control-Allow-Origin", "Content-Type", "Accept", "Authorization")
+                .exposedHeaders("Access-Control-Allow-Origin", "Content-Type", "Accept", "Authorization")
+                .allowCredentials(true).maxAge(3600);
+    }
+}
