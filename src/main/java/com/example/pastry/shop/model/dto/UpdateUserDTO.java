267b19373fdf69757c229b282247032c213dcb61
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-06-21 16:06:23 +0300

    Refactoring

--- src/main/java/com/example/pastry/shop/model/dto/UpdateUserDTO.java
+++ src/main/java/com/example/pastry/shop/model/dto/UpdateUserDTO.java
@@ -30,16 +30,8 @@ public class UpdateUserDTO {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
     public String getLastName() {
         return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
 
     public String getEmail() {
