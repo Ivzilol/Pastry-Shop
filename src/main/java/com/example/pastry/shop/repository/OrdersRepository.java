5bc68e4e74930c0a2f7c1a0ab1cdebabc6fcf584
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-06-22 21:52:22 +0300

    Refactoring

--- src/main/java/com/example/pastry/shop/repository/OrdersRepository.java
+++ src/main/java/com/example/pastry/shop/repository/OrdersRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 import java.util.Set;
 
 @Repository
@@ -22,15 +21,10 @@ public interface OrdersRepository extends JpaRepository<Orders, Long> {
             " where o.status = 'confirmed'")
     Set<Orders> findByStatus();
 
-    Set<Orders> findByUsers_Id(Long id);
-
     @Query("select o from Orders as o")
     Set<Orders> findAllOrders();
 
     Set<Orders> findByKeyOrderProduct(Long id);
-    @Query("select o.users from Orders as o" +
-            " where o.keyOrderProduct = :id")
-    Optional<Orders> findUsersId(Long id);
 
     @Query("select o from Orders as o" +
             " where o.users.id = :id")
