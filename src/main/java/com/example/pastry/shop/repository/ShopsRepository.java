7f5ab33507ae266146f291cf347b9ab5a4b8e173
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-06-22 21:53:03 +0300

    Refactoring

--- src/main/java/com/example/pastry/shop/repository/ShopsRepository.java
+++ src/main/java/com/example/pastry/shop/repository/ShopsRepository.java
@@ -3,7 +3,6 @@ package com.example.pastry.shop.repository;
 import com.example.pastry.shop.model.entity.Shops;
 import com.example.pastry.shop.model.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -13,10 +12,6 @@ import java.util.Set;
 public interface ShopsRepository extends JpaRepository<Shops, Long> {
 
     Set<Shops> findByUsers(Users user);
-
-    @Query("select s from Shops as s " +
-            "where s.admin = :admin or s.status is not null " )
-    Set<Shops> findByAdmin(Users admin);
 
     Optional<Shops> findByName(String name);
 }
