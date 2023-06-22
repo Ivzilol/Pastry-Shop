6ee5412287a9da84a901b147375c4457e19e9f0b
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-06-22 21:51:00 +0300

    Refactoring

--- src/main/java/com/example/pastry/shop/repository/OrdersProcessingRepository.java
+++ src/main/java/com/example/pastry/shop/repository/OrdersProcessingRepository.java
@@ -1,8 +1,6 @@
 package com.example.pastry.shop.repository;
 
-import com.example.pastry.shop.model.entity.Orders;
 import com.example.pastry.shop.model.entity.OrdersProcessing;
-import com.example.pastry.shop.model.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
