f895d7aa30c5fdef5e1faa9bd5d5d699a1bd8446
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-08-07 21:46:57 +0300

    ;

--- src/main/java/com/example/pastry/shop/util/CustomPasswordEncoder.java
+++ src/main/java/com/example/pastry/shop/util/CustomPasswordEncoder.java
@@ -1,15 +1,16 @@
 package com.example.pastry.shop.util;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
 public class CustomPasswordEncoder {
 
 
     private PasswordEncoder passwordEncoder;
+
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
