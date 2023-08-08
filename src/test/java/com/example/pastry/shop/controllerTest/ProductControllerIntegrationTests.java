b3b0836140c9ab14fbda6e1665a6ad92ca0bdd25
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-08-08 16:50:32 +0300

    Integration Tests

--- src/test/java/com/example/pastry/shop/controllerTest/ProductControllerIntegrationTests.java
+++ src/test/java/com/example/pastry/shop/controllerTest/ProductControllerIntegrationTests.java
@@ -2,7 +2,9 @@ package com.example.pastry.shop.controllerTest;
 
 import com.example.pastry.shop.model.dto.CreateProductDTO;
 import com.example.pastry.shop.model.entity.Products;
+import com.example.pastry.shop.model.entity.Users;
 import com.example.pastry.shop.testRepository.TestH2RepositoryProducts;
+import com.example.pastry.shop.testRepository.TestH2RepositoryUsers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,6 +22,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -36,6 +39,9 @@ public class ProductControllerIntegrationTests {
 
     @Autowired
     private TestH2RepositoryProducts testH2RepositoryProducts;
+
+    @Autowired
+    private TestH2RepositoryUsers testH2RepositoryUsers;
 
     @BeforeAll
     public static void init() {
@@ -94,5 +100,9 @@ public class ProductControllerIntegrationTests {
     public void testGetUserLikes() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get(baseUrl + "/likes"))
                 .andExpect(MockMvcResultMatchers.status().isOk());
+        Long userId = 2L;
+        Optional<Users> user = testH2RepositoryUsers.findById(userId);
+        Set<Products> productLikes = user.get().getLikeProducts();
+        Assertions.assertEquals(0, productLikes.size());
     }
 }
