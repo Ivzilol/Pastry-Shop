cdcecc4fe9b40a2cf101e877e42b4b22b6852d19
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-08-26 20:35:25 +0300

    Email registration

---
+++ src/main/java/com/example/pastry/shop/config/JasyptConfiguration.java
@@ -0,0 +1,26 @@
+package com.example.pastry.shop.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfiguration {
+
+    @Bean(name = "encryptorBean")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword("bbGGbb1234");
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+}
---
+++ src/test/java/JasyptEncryptedTest.java
@@ -0,0 +1,26 @@
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.junit.jupiter.api.Test;
+
+public class JasyptEncryptedTest {
+
+    @Test
+    public void testEncryptString() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword("bbGGbb1234");
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+
+        String plantText = "bbGGbb1234";
+        String encryptedText = encryptor.encrypt(plantText);
+        System.out.println(encryptedText);
+    }
+
+
+}
