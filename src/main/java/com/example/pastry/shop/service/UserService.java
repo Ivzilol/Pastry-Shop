f6c5ebcdc3545fdc9a583a2d909bafa1b89c452b
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-08-27 13:48:47 +0300

    Email registration

--- src/main/java/com/example/pastry/shop/service/UserService.java
+++ src/main/java/com/example/pastry/shop/service/UserService.java
@@ -13,7 +13,6 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import net.bytebuddy.utility.RandomString;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMailMessage;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
