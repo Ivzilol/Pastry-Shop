cac8301d1e819b4c62f2255fd0e707d2fcbd7626
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-08-27 13:44:08 +0300

    Email registration

--- src/pastry-shob-web/src/components/Register/ConfirmRegister.js
+++ src/pastry-shob-web/src/components/Register/ConfirmRegister.js
@@ -1,9 +1,10 @@
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import {useEffect} from "react";
 import ajax from "../../Services/FetchService";
 
 const ConfirmRegister = () => {
     const {verificationCode} = useParams();
+    let navigate = useNavigate();
     const baseUrl = "http://localhost:8080/";
 
     useEffect(() => {
@@ -11,8 +12,17 @@ const ConfirmRegister = () => {
             .then()
     }, [verificationCode])
 
-    return(
-        <main>You have successfully activated your account</main>
+    return (
+        <main className="validation-form">
+            <h2>You have successfully activated your account</h2>
+            <h6>Please login with your account</h6>
+            <button
+                onClick={() => {
+                    navigate("/login")
+                }}
+            >Login
+            </button>
+        </main>
     )
 }
 export default ConfirmRegister;
