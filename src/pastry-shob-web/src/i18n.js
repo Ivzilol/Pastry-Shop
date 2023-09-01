e62a4704c27c0946e6860ace53ecf57a6c18709a
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-09-01 20:07:05 +0300

    Refactor

--- src/pastry-shob-web/src/i18n.js
+++ src/pastry-shob-web/src/i18n.js
@@ -3,10 +3,10 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
 // don't want to use this?
 // have a look at the Quick start guide
 // for passing in lng and translations on init
+
 
 i18n
     // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
@@ -15,7 +15,7 @@ i18n
     .use(Backend)
     // detect user language
     // learn more: https://github.com/i18next/i18next-browser-languageDetector
-    .use(LanguageDetector)
+    // .use(LanguageDetector)
     // pass the i18n instance to react-i18next.
     .use(initReactI18next)
     // init i18next
@@ -23,12 +23,7 @@ i18n
     .init({
         fallbackLng: 'en',
         debug: true,
-
         interpolation: {
             escapeValue: false, // not needed for react as it escapes by default
         }
-    });
-
-
-export default i18n;
-
+    })
