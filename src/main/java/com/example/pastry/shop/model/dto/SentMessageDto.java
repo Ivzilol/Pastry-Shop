914ecce7870b1d2a7c71a80ce2b81b363611c217
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-09-21 21:30:27 +0300

    Web Sockets for add chat room

--- src/main/java/com/example/pastry/shop/model/dto/SentMessageDto.java
+++ src/main/java/com/example/pastry/shop/model/dto/SentMessageDto.java
@@ -1,4 +1,14 @@
 package com.example.pastry.shop.model.dto;
 
 public class SentMessageDto {
+
+    public String newMessage;
+
+    public String getNewMessage() {
+        return newMessage;
+    }
+
+    public void setNewMessage(String newMessage) {
+        this.newMessage = newMessage;
+    }
 }
--- src/pastry-shob-web/src/components/ChatRoom/ChatRoom.js
+++ src/pastry-shob-web/src/components/ChatRoom/ChatRoom.js
@@ -1,19 +1,21 @@
 import {useUser} from "../../UserProvider/UserProvider";
 import {useEffect, useState} from "react";
 import ajax from "../../Services/FetchService";
+import {useNavigate} from "react-router-dom";
 
 const ChatRoom = () => {
 
     const user = useUser();
     const baseUrl = "http://localhost:8080/";
-    const [message, setMessage] = useState(null);
-
+    const [messages, setMessages] = useState(null);
+    const [newMessage, setNewMessage] = useState("")
+    const navigate = useNavigate();
 
     function getMessage() {
         if (user.jwt) {
             ajax(`${baseUrl}api/chatroom`, "GET", user.jwt)
                 .then(result => {
-                    setMessage(result)
+                    setMessages(result)
                 })
         }
     }
@@ -24,14 +26,54 @@ const ChatRoom = () => {
 
     function sentMessage() {
         if (user.jwt) {
+            const requestBody = {
+                newMessage: newMessage
+            }
+            ajax(`${baseUrl}api/chatroom/send`, "POST", user.jwt, requestBody)
+                .then((response) => {
+                    if (response !== undefined) {
+                        return null;
+                    } else {
+                        return alert("")
+                    }
+                })
+        } else {
+            navigate("/login")
         }
     }
 
 
     return (
-        <div>
-            Proba
-        </div>
+        <main className="chat">
+            <section className="chat-container">
+                <h1>Chat</h1>
+                {messages ? (
+                    <div className="chat-container-messages">
+                        {messages.map((message) => (
+                            <p>{message.message}</p>
+                        ))}
+                    </div>
+                ) : (
+                    <></>
+                )}
+                <div className="chat-container-input">
+                    <input
+                        type="text"
+                        name="message"
+                        placeholder="Напиешете съобщение"
+                        value={newMessage}
+                        onChange={(e) => setNewMessage(e.target.value)}
+                    />
+                    <button
+                        id="submit"
+                        type="button"
+                        onClick={() => sentMessage()}
+                    >
+                        Send
+                    </button>
+                </div>
+            </section>
+        </main>
     )
 }
 
