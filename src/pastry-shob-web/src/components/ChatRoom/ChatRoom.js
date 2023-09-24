fa65f5568cf0735807522899eb59770c3aa87218
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-09-24 20:19:55 +0300

    Chat Room

--- src/pastry-shob-web/src/components/ChatRoom/ChatRoom.js
+++ src/pastry-shob-web/src/components/ChatRoom/ChatRoom.js
@@ -12,6 +12,7 @@ const ChatRoom = () => {
     const navigate = useNavigate();
     const lastMessageRef = useRef(null);
     const [showMessage, setShowMessage] = useState(true);
+    const [isVisible, setIsVisible] = useState(true);
 
     useEffect(() => {
         if (lastMessageRef.current) {
@@ -57,56 +58,138 @@ const ChatRoom = () => {
 
     function handleClickCloseMessage() {
         setShowMessage(false)
+        setIsVisible(false)
     }
 
     function handleClickOpenMessage() {
         getMessage()
         setShowMessage(true)
+        setIsVisible(true)
     }
 
 
     return (
-        <div className="chat-container">
-            <h3>Пишете ни</h3>
-            <a
-                className="close"
-                id="submit"
-                type="submit"
-                onClick={handleClickCloseMessage}
-            >X</a>
-            {showMessage &&
-                <div className="chat-container-messages"
-                     style={{marginTop: '40px', overflowY: 'scroll', height: '60%'}}>
-                    {messages ? (
-                        messages.map((message) => (
-                            <div key={message.id} ref={lastMessageRef}>
-                                <p>{message.message}</p>
-                            </div>
-                        ))
-                    ) : (
-                        <></>
-                    )}
+        <main>
+            {isVisible ?
+                <div className="chat-container" style={{
+                    position: 'fixed',
+                    right: 0,
+                    width: '27%',
+                    height: '67%',
+                    backgroundColor: 'blue',
+                    zIndex: '9999',
+                    overflow: 'auto',
+                    padding: '20px',
+                    color: 'black',
+                    fontStyle: 'italic',
+                    borderRadius: '10px',
+                    bottom: 0,
+                    marginRight: '50px'
+                }}>
+                    <section className="chat-container-title">
+                        <h3>Пишете ни</h3>
+                        <button
+                            className="chat-container-close"
+                            id="submit"
+                            type="submit"
+                            onClick={handleClickCloseMessage}
+                        >X
+                        </button>
+                    </section>
+                    {showMessage &&
+                        <div className="chat-container-messages"
+                             style={{marginTop: '40px', overflowY: 'scroll', height: '60%'}}>
+                            {messages ? (
+                                messages.map((message) => (
+                                    <div key={message.id} ref={lastMessageRef}>
+                                        <p key={message.id}>{message.message}</p>
+                                    </div>
+                                ))
+                            ) : (
+                                <></>
+                            )}
 
+                        </div>
+                    }
+                    <div className="chat-container-input" style={{marginTop: '20px'}}>
+                        <input
+                            type="text"
+                            name="message"
+                            placeholder="Напиешете съобщение"
+                            value={newMessage}
+                            onChange={(e) => setNewMessage(e.target.value)}
+                            onFocus={handleClickOpenMessage}
+                        />
+                        <button
+                            id="submit"
+                            type="button"
+                            onClick={() => sentMessage()}
+                        >
+                            Send
+                        </button>
+                    </div>
+                </div>
+                :
+                <div className="chat-container" style={{
+                    position: 'fixed',
+                    right: 0,
+                    width: '27%',
+                    height: '27%',
+                    backgroundColor: 'blue',
+                    zIndex: '9999',
+                    overflow: 'auto',
+                    padding: '20px',
+                    color: 'black',
+                    fontStyle: 'italic',
+                    borderRadius: '10px',
+                    bottom: 0,
+                    marginRight: '50px'
+                }}>
+                    <section className="chat-container-title">
+                        <h3>Пишете ни</h3>
+                        <button
+                            className="chat-container-close"
+                            id="submit"
+                            type="submit"
+                            onClick={handleClickCloseMessage}
+                        >X
+                        </button>
+                    </section>
+                    {showMessage &&
+                        <div className="chat-container-messages"
+                             style={{marginTop: '40px', overflowY: 'scroll', height: '60%'}}>
+                            {messages ? (
+                                messages.map((message) => (
+                                    <div key={message.id} ref={lastMessageRef}>
+                                        <p key={message.id}>{message.message}</p>
+                                    </div>
+                                ))
+                            ) : (
+                                <></>
+                            )}
+
+                        </div>
+                    }
+                    <div className="chat-container-input" style={{marginTop: '20px'}}>
+                        <input
+                            type="text"
+                            name="message"
+                            placeholder="Напиешете съобщение"
+                            value={newMessage}
+                            onChange={(e) => setNewMessage(e.target.value)}
+                            onFocus={handleClickOpenMessage}
+                        />
+                        <button
+                            id="submit"
+                            type="button"
+                            onClick={() => sentMessage()}
+                        >
+                            Send
+                        </button>
+                    </div>
                 </div>
             }
-            <div className="chat-container-input" style={{marginTop: '20px'}}>
-                <input
-                    type="text"
-                    name="message"
-                    placeholder="Напиешете съобщение"
-                    value={newMessage}
-                    onChange={(e) => setNewMessage(e.target.value)}
-                    onFocus={handleClickOpenMessage}
-                />
-                <button
-                    id="submit"
-                    type="button"
-                    onClick={() => sentMessage()}
-                >
-                    Send
-                </button>
-            </div>
-        </div>
+        </main>
     )
 }
 
