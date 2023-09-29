f64c7dfbe6b8907cffb45c1ba79b7abb5e539182
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-09-29 21:16:27 +0300

    Refactor

--- src/pastry-shob-web/src/components/Orders/UserOrderTracking.js
+++ src/pastry-shob-web/src/components/Orders/UserOrderTracking.js
@@ -8,6 +8,7 @@ const UserOrderTracking = () => {
 
     const user = useUser();
     const [order, setOrder] = useState(null);
+    const [status, setStatus] = useState(null)
     const now = new Date();
     const hours = now.getHours();
     let promotion = false;
@@ -23,6 +24,12 @@ const UserOrderTracking = () => {
         ajax(`${baseUrl}api/orders/tracking`, "GET", user.jwt)
             .then(orderData => {
                 setOrder(orderData)
+                if (orderData.length > 0) {
+                    // eslint-disable-next-line array-callback-return
+                    orderData.map((order) => {
+                        setStatus(order.status)
+                    })
+                }
             });
     }, [user.jwt]);
 
@@ -53,13 +60,17 @@ const UserOrderTracking = () => {
                                                 allPriceProcessing += currentOrder.price - (currentOrder.price * 0.20)
                                                 :
                                                 allPriceProcessing += currentOrder.price
-                                        }
-                                        </p>
+                                        }</p>
                                     </div>
                                     :
                                     <></>
                             ))}
-                            <h4>{t('tracking-main-section.all-price')} {allPriceProcessing.toFixed(2)} {t('products-users.currency')}</h4>
+                            {order.length > 0 && status === 'confirmed'
+                                ?
+                                <h4>{t('tracking-main-section.all-price')} {allPriceProcessing.toFixed(2)} {t('products-users.currency')}</h4>
+                                :
+                                <h4>Вие нямате порчки в процес на приготвяне</h4>
+                            }
                         </article>
                     ) : (
                         <div></div>
@@ -93,7 +104,12 @@ const UserOrderTracking = () => {
                                     :
                                     <></>
                             ))}
-                            <h4>{t('tracking-main-section.all-price')} {allPriceSend.toFixed(2)} {t('products-users.currency')}</h4>
+                            {status === 'sent'
+                                ?
+                                <h4>{t('tracking-main-section.all-price')} {allPriceSend.toFixed(2)} {t('products-users.currency')}</h4>
+                                :
+                                <h4>Вие нямате поръчка в процес на доставка</h4>
+                            }
                         </article>
                     ) : (
                         <></>
