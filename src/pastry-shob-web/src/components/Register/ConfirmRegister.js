c8760e14c1c2f7aefe84285b8582d11a5d5c1a28
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-11-03 10:19:13 +0200

    Refactoring

--- src/pastry-shob-web/src/components/Register/ConfirmRegister.js
+++ src/pastry-shob-web/src/components/Register/ConfirmRegister.js
@@ -1,14 +1,15 @@
 import {useNavigate, useParams} from "react-router-dom";
 import {useEffect} from "react";
 import ajax from "../../Services/FetchService";
+import baseURL from "../BaseURL/BaseURL";
 
 const ConfirmRegister = () => {
     const {verificationCode} = useParams();
     let navigate = useNavigate();
-    const baseUrl = "http://localhost:8080/";
+
 
     useEffect(() => {
-        ajax(`${baseUrl}api/users/register/verify/${verificationCode}`, "POST", null, verificationCode)
+        ajax(`${baseURL}api/users/register/verify/${verificationCode}`, "POST", null, verificationCode)
             .then(response => {
                 if (response.custom === 'Successful activate your profile') {
                     alert(response.custom);
