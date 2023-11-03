d60c4a30a36f4bd4b61946aecac4077aeaa8905d
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-11-03 10:34:54 +0200

    Refactoring

--- src/pastry-shob-web/src/components/Products/ProductPies.js
+++ src/pastry-shob-web/src/components/Products/ProductPies.js
@@ -4,14 +4,14 @@ import ajax from "../../Services/FetchService";
 import {useNavigate} from "react-router-dom";
 import NavBar from "../NavBar/NavBar";
 import Footer from "../Footer/Footer";
+import baseURL from "../BaseURL/BaseURL";
 
 const ProductPies = () => {
     const user = useUser()
     const [pies, setPies] = useState(null);
     let navigate = useNavigate();
-    const baseUrl = "http://localhost:8080/";
     useEffect(() => {
-        ajax(`${baseUrl}api/products/pies`, "GET", user.jwt)
+        ajax(`${baseURL}api/products/pies`, "GET", user.jwt)
             .then(piesData => {
                 setPies(piesData)
             })
