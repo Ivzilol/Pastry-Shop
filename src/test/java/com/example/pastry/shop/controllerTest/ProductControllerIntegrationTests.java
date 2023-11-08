ee90545869b897ad7e26bbbe732942680af1984e
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-11-08 19:22:03 +0200

    Integration Tests

--- src/test/java/com/example/pastry/shop/controllerTest/ProductControllerIntegrationTests.java
+++ src/test/java/com/example/pastry/shop/controllerTest/ProductControllerIntegrationTests.java
@@ -5,6 +5,7 @@ import com.example.pastry.shop.model.entity.Products;
 import com.example.pastry.shop.model.entity.Users;
 import com.example.pastry.shop.testRepository.TestH2RepositoryProducts;
 import com.example.pastry.shop.testRepository.TestH2RepositoryUsers;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,15 +15,22 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -56,23 +64,28 @@ public class ProductControllerIntegrationTests {
 
     @Test
     @WithUserDetails("Tosho")
-    public void testCreateProduct() {
-        CreateProductDTO createProductDTO = new CreateProductDTO();
-        createProductDTO.setName("Баница");
-        createProductDTO.setPrice(21.99);
-        createProductDTO.setDescription("Най Вкусната баница");
-        createProductDTO.setCategories("pie");
-//        createProductDTO.setImageUrl("https://i.ibb.co/QM6sBZh/333679583-1370979030385235-8353098425062243540-n.jpg");
-        createProductDTO.setShopName("Sladcarnicata na Mama");
-        Products result = restTemplate.postForObject(baseUrl + "/create/admin", createProductDTO, Products.class);
-        Assertions.assertEquals("Баница", result.getName());
+    public void testCreateProduct() throws Exception {
+        MockMultipartFile imageUrl = new MockMultipartFile("imageUrl",
+                "333679583_1370979030385235_8353098425062243540_n.jpg",
+                "multipart/form-data",
+                "333679583_1370979030385235_8353098425062243540_n.jpg".getBytes(StandardCharsets.UTF_8)
+                );
+        MockMultipartFile jsonFile = new MockMultipartFile("dto",
+                "",
+                "application/json",
+                "{\"name\": \"Баница\", \"description\": \"Най Вкусната баница\", \"categories\": \"pie\", \"shopName\": \"Sladcarnicata na Mama\", \"price\": \"21.99\"}".getBytes());
+//        String jsonRequest = new ObjectMapper().writeValueAsString(dto);
+        mockMvc.perform(MockMvcRequestBuilders.multipart(baseUrl + "/create/admin")
+                        .file(imageUrl)
+                        .file(jsonFile))
+                .andExpect(status().isOk());
     }
 
     @Test
     @WithUserDetails("Victor")
     public void testGetProductByUser() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get(baseUrl))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .andExpect(status().isOk());
         List<Products> allProducts = testH2RepositoryProducts.findAll();
         Assertions.assertEquals(1, allProducts.size());
     }
@@ -81,7 +94,7 @@ public class ProductControllerIntegrationTests {
     public void testGetProductBId() throws Exception {
         Long productId = 3L;
         mockMvc.perform(MockMvcRequestBuilders.get(baseUrl + "/{productId}", productId))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .andExpect(status().isOk());
         Optional<Products> productById = testH2RepositoryProducts.findById(productId);
         Assertions.assertEquals("Баница", productById.get().getName());
     }
@@ -99,7 +112,7 @@ public class ProductControllerIntegrationTests {
     @WithUserDetails("Victor")
     public void testGetUserLikes() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get(baseUrl + "/likes"))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .andExpect(status().isOk());
         Long userId = 2L;
         Optional<Users> user = testH2RepositoryUsers.findById(userId);
         Set<Products> productLikes = user.get().getLikeProducts();
@@ -109,7 +122,7 @@ public class ProductControllerIntegrationTests {
     @Test
     public void testGetPies() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get(baseUrl + "/pies"))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .andExpect(status().isOk());
         Set<Products> pies = testH2RepositoryProducts.findAllPies();
         Assertions.assertEquals(1, pies.size());
     }
