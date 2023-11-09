9a61de27582c268126d27f85dce22604859282a7
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-11-09 20:27:44 +0200

    Integration Tests

--- src/main/java/com/example/pastry/shop/repository/OrdersRepository.java
+++ src/main/java/com/example/pastry/shop/repository/OrdersRepository.java
@@ -2,7 +2,6 @@ package com.example.pastry.shop.repository;
 
 import com.example.pastry.shop.model.dto.OrdersDTO;
 import com.example.pastry.shop.model.entity.Orders;
-import com.example.pastry.shop.model.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
