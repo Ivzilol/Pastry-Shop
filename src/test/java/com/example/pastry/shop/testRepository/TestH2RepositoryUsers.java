5d04a8588a96e55ab81740e789e7ae91ade7a060
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-11-16 15:24:37 +0200

    Refactor

--- src/test/java/com/example/pastry/shop/testRepository/TestH2RepositoryUsers.java
+++ src/test/java/com/example/pastry/shop/testRepository/TestH2RepositoryUsers.java
@@ -2,7 +2,6 @@ package com.example.pastry.shop.testRepository;
 
 import com.example.pastry.shop.model.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface TestH2RepositoryUsers extends JpaRepository<Users, Long> {
 
