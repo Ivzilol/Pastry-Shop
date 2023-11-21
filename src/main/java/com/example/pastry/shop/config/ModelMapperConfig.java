92b397eeddedfa2b6d03a46fc5a42e18006fe622
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-11-21 19:11:46 +0200

    Add ModelMapper

--- src/main/java/com/example/pastry/shop/config/ModelMapperConfig.java
+++ src/main/java/com/example/pastry/shop/config/ModelMapperConfig.java
@@ -1,10 +1,7 @@
 package com.example.pastry.shop.config;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.spi.MappingContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
