a97e277f4f6285ef7c398b31126dd107e31f21ed
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-11-21 21:31:40 +0200

    Add not found page.

--- src/pastry-shob-web/src/components/NotFoundPage/NotFoundPage.js
+++ src/pastry-shob-web/src/components/NotFoundPage/NotFoundPage.js
@@ -2,11 +2,13 @@ import {Link} from "react-router-dom";
 
 const NotFoundPage = () => {
     return (
-        <div>
-            <h1>Oops.....</h1>
-            <h2>Page not found!</h2>
-            <p>Go to the <Link to="/">Homepage</Link></p>
-        </div>
+        <main className="not-found">
+            <div>
+                <h1>Oops.....</h1>
+                <h2>Page not found!</h2>
+                <p>Go to the <Link className="not-found-link" to="/">Homepage</Link></p>
+            </div>
+        </main>
     )
 }
 
