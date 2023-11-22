15cfd14d086090fd47e02a30b43a5ac3931106f6
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-11-22 17:50:42 +0200

    Add Global Error

--- src/pastry-shob-web/src/components/Products/ProductsUser.js
+++ src/pastry-shob-web/src/components/Products/ProductsUser.js
@@ -12,6 +12,7 @@ import jwt_decode from "jwt-decode";
 import OrderWindow from "../Orders/OrderWindow";
 import {useTranslation} from "react-i18next";
 import baseURL from "../BaseURL/BaseURL";
+import GlobalError from "../GlobalError/GlobalError";
 
 
 const ProductsUser = () => {
@@ -22,6 +23,7 @@ const ProductsUser = () => {
     let navigate = useNavigate();
     const {t} = useTranslation();
     const [orderWindow, setOrderWindow] = useState(false);
+    const [globalError, setGlobalError] = useState(false);
     const [product, setProduct] = useState({
             name: "",
             price: null,
@@ -34,7 +36,6 @@ const ProductsUser = () => {
     useEffect(() => {
         ajax(`${baseURL}api/products`, "GET", user.jwt)
             .then(productsData => {
-                console.log(productsData)
                 setProducts(productsData);
             });
         if (user.jwt) {
@@ -64,7 +65,6 @@ const ProductsUser = () => {
                 setOrderDialog(true);
                 timerOrderWindow();
             });
-
     }
 
     function orderProducts(id) {
@@ -72,6 +72,9 @@ const ProductsUser = () => {
             .then(productData => {
                 setProduct(productData);
                 setOrderWindow(true);
+            })
+            .catch(() => {
+                setGlobalError(true)
             })
     }
 
@@ -158,84 +161,87 @@ const ProductsUser = () => {
     }
 
     return (
-        <main className="products-users">
-            <NavBar/>
-            {orderWindow ? <OrderWindow/> : <></>}
-            {orderDialog &&
-                <div className="home-page-order-dialog">
-                    <h4>{t('products-users.choice')}</h4>
-                    <h5>{t('products-users.name')} {orderDialogProductName}</h5>
-                    <p>{t('products-users.price')} {orderDialogProductDescription} {t('products-users.currency')}</p>
-                </div>
-            }
-            {products ? (
-                <article className="products-container">
-                    {products.map((product) => (
-                        <div
-                            className="products-container-items"
-                            key={product.id}
-                            id={product.id}
-                        >
-                            <p className="products-container-item"
-                            >{t('products-users.name')} {product.name}</p>
-                            <p className="products-container-item"
-                            >{t('products-users.price')} {product.price} {t('products-users.currency')}</p>
-                            <p className="products-container-item-description"
-                            >{t('products-users.description')} {product.description}</p>
-                            <img
-                                className="product-img" src={product.imageUrl} alt="new"
-                            />
-                            <div className="products-container-item-likes-container">
-                                <p className="products-container-item-likes">
-                                    <FontAwesomeIcon icon={faThumbsUp}
-                                                     className="products-container-item-likes-icon"
-                                    />
-                                    {Number(product.userLikes.length)}</p>
-                                <div className="products-container-item-likes-container-buttons">
-                                    {/* eslint-disable-next-line array-callback-return */}
-                                    {getIsLike(product)}
-                                    {isEquals
-                                        ?
+        <>
+            {globalError ? <GlobalError/> : <main className="products-users">
+                <NavBar/>
+                {orderWindow ? <OrderWindow/> : <></>}
+                {orderDialog &&
+                    <div className="home-page-order-dialog">
+                        <h4>{t('products-users.choice')}</h4>
+                        <h5>{t('products-users.name')} {orderDialogProductName}</h5>
+                        <p>{t('products-users.price')} {orderDialogProductDescription} {t('products-users.currency')}</p>
+                    </div>
+                }
+                {products ? (
+                    <article className="products-container">
+                        {products.map((product) => (
+                            <div
+                                className="products-container-items"
+                                key={product.id}
+                                id={product.id}
+                            >
+                                <p className="products-container-item"
+                                >{t('products-users.name')} {product.name}</p>
+                                <p className="products-container-item"
+                                >{t('products-users.price')} {product.price} {t('products-users.currency')}</p>
+                                <p className="products-container-item-description"
+                                >{t('products-users.description')} {product.description}</p>
+                                <img
+                                    className="product-img" src={product.imageUrl} alt="new"
+                                />
+                                <div className="products-container-item-likes-container">
+                                    <p className="products-container-item-likes">
+                                        <FontAwesomeIcon icon={faThumbsUp}
+                                                         className="products-container-item-likes-icon"
+                                        />
+                                        {Number(product.userLikes.length)}</p>
+                                    <div className="products-container-item-likes-container-buttons">
+                                        {/* eslint-disable-next-line array-callback-return */}
+                                        {getIsLike(product)}
+                                        {isEquals
+                                            ?
+                                            <button
+                                                className="products-container-item-likes-button"
+                                                id="submit"
+                                                type="button"
+                                                onClick={() => dislikeProduct(product.id)}
+                                            >{t('products-users.no-like-button')}
+                                            </button>
+                                            :
+                                            <button
+                                                className="products-container-item-likes-button"
+                                                id="submit"
+                                                type="button"
+                                                onClick={() => likeProduct(product.id)}
+                                            >{t('products-users.like-button')}
+                                            </button>
+                                        }
                                         <button
-                                            className="products-container-item-likes-button"
+                                            className="products-container-item-likes-container-button2"
                                             id="submit"
                                             type="button"
-                                            onClick={() => dislikeProduct(product.id)}
-                                        >{t('products-users.no-like-button')}
+                                            onClick={() => {
+                                                orderProduct(product.id);
+                                                orderProducts(product.id);
+                                            }}
+                                        >
+                                            {t('products-users.order-button')}
                                         </button>
-                                        :
-                                        <button
-                                            className="products-container-item-likes-button"
-                                            id="submit"
-                                            type="button"
-                                            onClick={() => likeProduct(product.id)}
-                                        >{t('products-users.like-button')}
-                                        </button>
-                                    }
-                                    <button
-                                        className="products-container-item-likes-container-button2"
-                                        id="submit"
-                                        type="button"
-                                        onClick={() => {
-                                            orderProduct(product.id);
-                                            orderProducts(product.id);
-                                        }}
-                                    >
-                                        {t('products-users.order-button')}
-                                    </button>
+                                    </div>
+                                    <p className="counter-likes">
+                                        {isEquals = false}
+                                    </p>
                                 </div>
-                                <p className="counter-likes">
-                                    {isEquals = false}
-                                </p>
                             </div>
-                        </div>
-                    ))}
-                </article>
-            ) : (
-                <></>
-            )}
-            <Footer/>
-        </main>
+                        ))}
+                    </article>
+                ) : (
+                    <></>
+                )}
+                <Footer/>
+            </main>
+            }
+        </>
     )
 }
 export default ProductsUser;
