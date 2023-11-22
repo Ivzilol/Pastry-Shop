d6526a6fd485bd6145474e69031331d730fbd8c4
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-11-22 13:11:54 +0200

    Add global error.

--- src/pastry-shob-web/src/components/GlobalError/GlobalError.js
+++ src/pastry-shob-web/src/components/GlobalError/GlobalError.js
@@ -1,13 +1,13 @@
-
-
 const GlobalError = () => {
 
     return (
-        <div className="global-error">
-            <h1>Oops! Something went wrong.</h1>
-            <p>We now for the problem and we are working ot its removal!
-                Please try again later.</p>
-        </div>
+        <main className="error-global">
+            <div className="global-error">
+                <h1>Oops! Something went wrong.</h1>
+                <p>We now for the problem and we are working ot its removal!
+                    Please try again later.</p>
+            </div>
+        </main>
     );
 
 }
