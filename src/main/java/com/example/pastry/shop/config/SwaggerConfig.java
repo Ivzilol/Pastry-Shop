87fcdae52797722b7ebd67a7feae140be63f2b15
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-11-23 17:54:09 +0200

    Refactoring

--- src/main/java/com/example/pastry/shop/config/SwaggerConfig.java
+++ src/main/java/com/example/pastry/shop/config/SwaggerConfig.java
@@ -2,18 +2,35 @@ package com.example.pastry.shop.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Pastry Shop", version = "v1"))
 @SecurityScheme(
-        name = "jwtBearerAuth",
+        name = "JWT Bearer Authorization",
         type = SecuritySchemeType.HTTP,
         bearerFormat = "JWT",
         scheme = "bearer"
 )
 public class SwaggerConfig {
 
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().info(
+                new Info().
+                        title("Pastry Shop").
+                        contact(
+                                new Contact().
+                                        email("ivailoali@gmail.com")
+                                        .name("Ivaylo Alichkov")
+
+                        ).
+                        description("Welcome to Mom's Sweet Shop, an app that simulates online " +
+                                "ordering and delivery of confectionery.")
+        );
+    }
 }
