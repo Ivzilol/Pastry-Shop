0225ef163c2c982ad8a76d8c3af658d65dc73804
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-11-23 18:18:58 +0200

    Refactoring

--- src/main/java/com/example/pastry/shop/config/SwaggerConfig.java
+++ src/main/java/com/example/pastry/shop/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package com.example.pastry.shop.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
