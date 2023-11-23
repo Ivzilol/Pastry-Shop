82b00ea8b2000ccb998a81fe355701c93d341180
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-11-23 12:20:57 +0200

    Refactor

--- src/pastry-shob-web/src/components/GlobalError/GlobalError.js
+++ src/pastry-shob-web/src/components/GlobalError/GlobalError.js
@@ -1,7 +1,10 @@
+import NavBar from "../NavBar/NavBar";
+
 const GlobalError = () => {
 
     return (
         <main className="error-global">
+            <NavBar/>
             <div className="global-error">
                 <h1>Oops! Something went wrong.</h1>
                 <p>We now for the problem and we are working ot its removal!
