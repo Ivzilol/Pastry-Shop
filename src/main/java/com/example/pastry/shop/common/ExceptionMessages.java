2b32f2b5b58325ea06a9f91c7941527e3b558a0c
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-11-24 16:32:02 +0200

    Refactor

--- src/main/java/com/example/pastry/shop/common/ExceptionMessages.java
+++ src/main/java/com/example/pastry/shop/common/ExceptionMessages.java
@@ -13,5 +13,11 @@ public class ExceptionMessages {
     public static final String USERNAME_EXIST_REGISTRATION_ERROR = "Username already exist";
     public static final String EMAIL_VALID_REGISTRATION_ERROR = "Please enter a valid email";
     public static final String PASSWORD_NOT_MATCH_REGISTRATION_ERROR = "Passwords must match";
+    public static final String EMPTY_PRODUCT = "Product name can not be empty!" ;
+    public static final String EMPTY_PRICE = "Product price can not be empty!";
+    public static final String NEGATIVE_NUMBER_PRICE = "Price must be positive number";
+    public static final String EMPTY_CATEGORY = "Product category can not be empty!";
+    public static final String EMPTY_DESCRIPTION = "Product description can not be empty!";
+    public static final String EMPTY_SHOP_NAME = "Shop name can not be empty!";
 
 }
--- src/main/java/com/example/pastry/shop/model/dto/CreateProductDTO.java
+++ src/main/java/com/example/pastry/shop/model/dto/CreateProductDTO.java
@@ -3,25 +3,35 @@ package com.example.pastry.shop.model.dto;
 import com.example.pastry.shop.validation.annotation.UniqueProductName;
 import com.unboundid.util.NotNull;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
+
+import static com.example.pastry.shop.common.ExceptionMessages.*;
+
 @Getter
 @Setter
 public class CreateProductDTO {
 
-    @NotBlank(message = "Product name can not be empty!")
-    @UniqueProductName(message = "Product with this name already exist")
+    @NotBlank(message = EMPTY_PRODUCT)
+    @UniqueProductName
     private String name;
-    @NotBlank(message = "Product price can not be empty!")
+
+    @Positive(message = NEGATIVE_NUMBER_PRICE)
+    @NotBlank(message = EMPTY_PRICE)
     private Double price;
-    @NotBlank(message = "Product category can not be empty!")
+
+    @NotBlank(message = EMPTY_CATEGORY)
     private String Categories;
-    @NotBlank(message = "Product description can not be empty!")
+
+    @NotBlank(message = EMPTY_DESCRIPTION)
     private String description;
+
     @NotNull
     private MultipartFile imageUrl;
-    @NotBlank(message = "Shop name can not be empty!")
+
+    @NotBlank(message = EMPTY_SHOP_NAME)
     private String shopName;
 
     public CreateProductDTO() {
