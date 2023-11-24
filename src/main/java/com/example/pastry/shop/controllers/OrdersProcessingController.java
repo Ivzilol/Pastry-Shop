6e009d211532bbaf65fa893866770ad55d3682f3
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-11-24 13:28:49 +0200

    Documentation

--- src/main/java/com/example/pastry/shop/controllers/OrdersProcessingController.java
+++ src/main/java/com/example/pastry/shop/controllers/OrdersProcessingController.java
@@ -1,8 +1,15 @@
 package com.example.pastry.shop.controllers;
 
+import com.example.pastry.shop.model.dto.OrdersDTO;
 import com.example.pastry.shop.model.entity.Orders;
 import com.example.pastry.shop.response.CustomResponse;
 import com.example.pastry.shop.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +28,15 @@ public class OrdersProcessingController {
         this.orderService = orderService;
     }
 
+    @Operation(summary = "Start processing order", security = {
+            @SecurityRequirement(name = "Authorization")
+    })
+    @ApiResponses(
+            value = {@ApiResponse(responseCode = "200", description = "Start processing order",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomResponse.class)))
+            }
+    )
     @PostMapping("/admin/{id}")
     public ResponseEntity<?> startProcessingOrder(@PathVariable Long id) {
         Set<Orders> currentOrders = this.orderService.findByUsersId(id);
