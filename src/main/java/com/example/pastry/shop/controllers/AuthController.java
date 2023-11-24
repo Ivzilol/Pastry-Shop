86905a9f31bdb0b8c9145e05b485d93ecd818c12
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-11-24 13:40:51 +0200

    Documentation

--- src/main/java/com/example/pastry/shop/controllers/AuthController.java
+++ src/main/java/com/example/pastry/shop/controllers/AuthController.java
@@ -6,6 +6,12 @@ import com.example.pastry.shop.model.entity.Users;
 import com.example.pastry.shop.service.UserService;
 import com.example.pastry.shop.util.JwtUtil;
 import io.jsonwebtoken.ExpiredJwtException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -30,16 +36,24 @@ public class AuthController {
     private final JwtUtil jwtUtil;
 
     private final UserService userService;
+
     public AuthController(AuthenticationManager authenticationManager, JwtUtil jwtUtil, UserService userService) {
         this.authenticationManager = authenticationManager;
         this.jwtUtil = jwtUtil;
         this.userService = userService;
     }
 
+    @Operation(summary = "User login")
+    @ApiResponses(
+            value = {@ApiResponse(responseCode = "200", description = "User is Authorise",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UsersDTO.class))),
+                    @ApiResponse(responseCode = "401", description = "User is not Authorise")}
+    )
     @PostMapping("login")
-    public ResponseEntity<?> login (@RequestBody AuthCredentialRequest request) {
+    public ResponseEntity<?> login(@RequestBody AuthCredentialRequest request) {
         Optional<Users> validateUser = ifValidate(request);
-        UsersDTO  usersDTO = this.userService.findCurrentUser(request.getUsername());
+        UsersDTO usersDTO = this.userService.findCurrentUser(request.getUsername());
         if (validateUser.isPresent()) {
             Authentication authenticate = authenticationManager
                     .authenticate(new UsernamePasswordAuthenticationToken(
@@ -61,9 +75,16 @@ public class AuthController {
         return this.userService.validate(request.getUsername());
     }
 
+    @Operation(summary = "Validate user token", security = {
+            @SecurityRequirement(name = "Authorization")
+    })
+    @ApiResponses(
+            value = {@ApiResponse(responseCode = "200", description = "User is Authorise"),
+                    @ApiResponse(responseCode = "401", description = "Not Authorise")}
+    )
     @GetMapping("/validate")
     public ResponseEntity<?> validateToken(@RequestParam String token,
-                                            @AuthenticationPrincipal Users user) {
+                                           @AuthenticationPrincipal Users user) {
         try {
             Boolean isTokenValid = jwtUtil.validateToken(token, user);
             return ResponseEntity.ok(isTokenValid);
