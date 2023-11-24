01705dbd4090d311ecfef4dd43fbcc4e477716a0
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-11-24 13:48:28 +0200

    Documentation

--- src/main/java/com/example/pastry/shop/controllers/ChatController.java
+++ src/main/java/com/example/pastry/shop/controllers/ChatController.java
@@ -3,9 +3,16 @@ package com.example.pastry.shop.controllers;
 import com.example.pastry.shop.model.dto.ChatMessageDTO;
 import com.example.pastry.shop.model.dto.Message;
 import com.example.pastry.shop.model.dto.UnansweredMessagesDTO;
+import com.example.pastry.shop.model.dto.UsersDTO;
 import com.example.pastry.shop.model.entity.Users;
 import com.example.pastry.shop.response.CustomResponse;
 import com.example.pastry.shop.service.MessageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -32,6 +39,7 @@ public class ChatController {
         this.simpMessagingTemplate = simpMessagingTemplate;
     }
 
+
     @MessageMapping("/message/{room}")
     public void receiveMessage(@Payload Message message, @DestinationVariable String room) {
         simpMessagingTemplate.convertAndSend("/chat-rooms/" + room, message);
@@ -42,18 +50,42 @@ public class ChatController {
         }
     }
 
+    @Operation(summary = "Get all user message", security = {
+            @SecurityRequirement(name = "Authorization")
+    })
+    @ApiResponses(
+            value = {@ApiResponse(responseCode = "200", description = "Get all user message",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UnansweredMessagesDTO.class)))}
+    )
     @GetMapping("api/chat-room/admin/all")
     public ResponseEntity<?> getAllUsersMessage(@AuthenticationPrincipal Users user) {
         Set<UnansweredMessagesDTO> unansweredMessagesDTO = this.messageService.getAllUsersMessages(user);
         return ResponseEntity.ok(unansweredMessagesDTO);
     }
 
+    @Operation(summary = "Get messaage from user")
+    @ApiResponses(
+            value = {@ApiResponse(responseCode = "200", description = "Get messaage from user",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ChatMessageDTO.class)))}
+    )
     @GetMapping("api/chat-room/get-messages/{room}")
     public ResponseEntity<?> getMessagesFromUser(@PathVariable String room) {
         Set<ChatMessageDTO> chatMessageDTO = this.messageService.findMessagesByUser(room);
         return ResponseEntity.ok(chatMessageDTO);
     }
 
+    @Operation(summary = "Admin make conversation finished", security = {
+            @SecurityRequirement(name = "Authorization")
+    })
+    @ApiResponses(
+            value = {@ApiResponse(responseCode = "202", description = "Admin make conversation finished"),
+                    @ApiResponse(description = "Invalid message",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = CustomResponse.class)))
+            }
+    )
     @PatchMapping("api/chat-room/admin/finish/{room}")
     public ResponseEntity<?> makeChatFinished(@AuthenticationPrincipal Users user,
                                               @PathVariable String room) {
