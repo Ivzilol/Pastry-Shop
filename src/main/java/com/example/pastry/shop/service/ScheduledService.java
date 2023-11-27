dd3d49897e86f3b1d95e879d919ae2b6d69f72ae
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-11-27 13:40:39 +0200

    Refactor

--- src/main/java/com/example/pastry/shop/service/ScheduledService.java
+++ src/main/java/com/example/pastry/shop/service/ScheduledService.java
@@ -1,8 +1,10 @@
 package com.example.pastry.shop.service;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface ScheduledService {
+    @Scheduled()
     void deleteFinishedChat();
 }
--- src/main/java/com/example/pastry/shop/service/UserTopClientService.java
+++ src/main/java/com/example/pastry/shop/service/UserTopClientService.java
@@ -2,12 +2,13 @@ package com.example.pastry.shop.service;
 
 import com.example.pastry.shop.events.UserTopClientEvent;
 import jakarta.mail.MessagingException;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 import java.io.UnsupportedEncodingException;
 
 @Service
 public interface UserTopClientService {
-
-   void UserTopClient(UserTopClientEvent event) throws MessagingException, UnsupportedEncodingException;
+    @EventListener(UserTopClientEvent.class)
+    void UserTopClient(UserTopClientEvent event) throws MessagingException, UnsupportedEncodingException;
 }
