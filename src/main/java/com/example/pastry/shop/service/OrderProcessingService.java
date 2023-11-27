2882fa098f72962f9fdb4f3b2868995910a7f713
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-11-27 12:25:03 +0200

    Refactor

--- src/main/java/com/example/pastry/shop/service/OrderProcessingService.java
+++ src/main/java/com/example/pastry/shop/service/OrderProcessingService.java
@@ -2,46 +2,18 @@ package com.example.pastry.shop.service;
 
 import com.example.pastry.shop.model.dto.OrdersProcessingDTO;
 import com.example.pastry.shop.model.entity.Users;
-import com.example.pastry.shop.model.enums.AuthorityEnum;
-import com.example.pastry.shop.repository.OrdersProcessingRepository;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
-public class OrderProcessingService {
+public interface OrderProcessingService {
 
-    private final OrdersProcessingRepository ordersProcessingRepository;
+    Set<OrdersProcessingDTO> findByStatus(Users user);
 
-    @Value("${status_send}")
-    private String statusSend;
+    Set<OrdersProcessingDTO> findOrdersCurrentUser(Users user);
 
-    public OrderProcessingService(OrdersProcessingRepository ordersProcessingRepository) {
-        this.ordersProcessingRepository = ordersProcessingRepository;
-    }
+    Set<OrdersProcessingDTO> getAllOrders();
 
 
-    public Set<OrdersProcessingDTO> findByStatus(Users user) {
-        boolean isAdmin = isAdmin(user);
-        if (isAdmin) {
-            return this.ordersProcessingRepository.findByStatus(statusSend);
-        } else {
-            return null;
-        }
-    }
-
-    private static boolean isAdmin(Users user) {
-        return user.getAuthorities()
-                .stream().anyMatch(auth -> AuthorityEnum.admin.name().equals(auth.getAuthority()));
-    }
-
-
-    public Set<OrdersProcessingDTO> findOrdersCurrentUser(Users user) {
-        return this.ordersProcessingRepository.findByUserIdOrderByDateOfReceipt(user.getId());
-    }
-
-    public Set<OrdersProcessingDTO> getAllOrders() {
-        return this.ordersProcessingRepository.findAllOrders();
-    }
 }
---
+++ src/main/java/com/example/pastry/shop/service/impl/OrderProcessingServiceImpl.java
@@ -0,0 +1,51 @@
+package com.example.pastry.shop.service.impl;
+
+import com.example.pastry.shop.model.dto.OrdersProcessingDTO;
+import com.example.pastry.shop.model.entity.Users;
+import com.example.pastry.shop.model.enums.AuthorityEnum;
+import com.example.pastry.shop.repository.OrdersProcessingRepository;
+import com.example.pastry.shop.service.OrderProcessingService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class OrderProcessingServiceImpl implements OrderProcessingService {
+
+    private final OrdersProcessingRepository ordersProcessingRepository;
+
+    @Value("${status_send}")
+    private String statusSend;
+
+
+    public OrderProcessingServiceImpl(OrdersProcessingRepository ordersProcessingRepository) {
+        this.ordersProcessingRepository = ordersProcessingRepository;
+    }
+
+
+    private static boolean isAdmin(Users user) {
+        return user.getAuthorities()
+                .stream().anyMatch(auth -> AuthorityEnum.admin.name().equals(auth.getAuthority()));
+    }
+
+    @Override
+    public Set<OrdersProcessingDTO> findByStatus(Users user) {
+        boolean isAdmin = isAdmin(user);
+        if (isAdmin) {
+            return this.ordersProcessingRepository.findByStatus(statusSend);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public Set<OrdersProcessingDTO> findOrdersCurrentUser(Users user) {
+        return this.ordersProcessingRepository.findByUserIdOrderByDateOfReceipt(user.getId());
+    }
+
+    @Override
+    public Set<OrdersProcessingDTO> getAllOrders() {
+        return this.ordersProcessingRepository.findAllOrders();
+    }
+}
