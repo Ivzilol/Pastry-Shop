a600c1aed2f347e073b4b1fad0308ed662580399
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-12-06 22:07:51 +0200

    Refactor

--- src/pastry-shob-web/src/UserProvider/UserProvider.js
+++ src/pastry-shob-web/src/UserProvider/UserProvider.js
@@ -5,7 +5,7 @@ const UserContext = createContext();
 
 
 const UserProvider = ({children}) => {
-    const [jwt, setJwt] = useLocalState("", "jwt");
+    const [jwt, setJwt] = useLocalState(null, "jwt");
     const value = {jwt, setJwt};
     return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 };
