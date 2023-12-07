cbcfacd0d22a2cf8ddc0d7f2741187eb52a72e61
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-12-07 13:56:54 +0200

    Refactor

--- src/main/java/com/example/pastry/shop/controllers/impl/HomeControllerImpl.java
+++ src/main/java/com/example/pastry/shop/controllers/impl/HomeControllerImpl.java
@@ -11,8 +11,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
