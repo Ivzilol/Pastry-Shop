d69af3d75cfb09fc4887975309f59a2785b4843a
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-12-08 19:45:58 +0200

    Refactor

--- src/main/java/com/example/pastry/shop/config/SecurityConfiguration.java
+++ src/main/java/com/example/pastry/shop/config/SecurityConfiguration.java
@@ -19,7 +19,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static org.springframework.security.config.Customizer.withDefaults;
