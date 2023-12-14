9109d24ae9b2c7804c59d19b4685ffbd5eddeba8
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-12-14 14:08:55 +0200

    Refactor

--- src/main/java/com/example/pastry/shop/service/ScheduledService.java
+++ src/main/java/com/example/pastry/shop/service/ScheduledService.java
@@ -1,7 +1,6 @@
 package com.example.pastry.shop.service;
 
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
 
 public interface ScheduledService {
     @Scheduled()
